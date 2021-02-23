3097c528e8a82b60e6fdaf5235b9ee4d22b576af
99003762 <78848692+99003762@users.noreply.github.com>
2021-02-23 14:55:58 +0530

    With user input for any word

--- Project.py
+++ Project.py
@@ -1,35 +1,18 @@
 import re
 count = 0
-software = []
+word = str(input())
+OUTPUT = []
 line = 0
-pattern = re.compile("software", re.I)
+pattern = re.compile(word, re.I)
 
 with open('Input_file.txt', 'rt') as file_info:
     for file_line in file_info:
         line += 1
-        if pattern.search(file_line) != None:
-            software.append((line, file_line.rstrip('\n')))
-    for answer in software:
+        if pattern.search(file_line) is not None:
+            OUTPUT.append((line, file_line.rstrip('\n')))
+    for answer in OUTPUT:
         count += 1
-        with open('software.txt', 'a') as file_answer:
+        with open('OUTPUT.txt', 'a') as file_answer:
             file_answer.writelines(str(count)+' :')
             file_answer.writelines(answer[1]+'\n')
-print(count)
-
-
-license = []
-count = 0
-pattern = re.compile("license", re.I)
-
-with open('Input_file.txt', 'rt') as file_info:
-    for file_line in file_info:
-        line += 1
-        if pattern.search(file_line) != None:
-            license.append((line, file_line.rstrip('\n')))
-    for answer in license:
-        count += 1
-        with open('license.txt', 'a') as file_answer:
-            file_answer.writelines(str(count)+' :')
-            file_answer.writelines(answer[1]+'\n')
-
 print(count)
