d9088cd2742d93c07f489ee768bf1d4bdea04290
99003762 <78848692+99003762@users.noreply.github.com>
2021-02-23 15:30:02 +0530

    final project
    with user defined inputs
    and individual output for every file

--- Project.py
+++ Project.py
@@ -1,18 +1,19 @@
 import re
-count = 0
-word = str(input())
-OUTPUT = []
-line = 0
-pattern = re.compile(word, re.I)
-
-with open('Input_file.txt', 'rt') as file_info:
-    for file_line in file_info:
-        line += 1
-        if pattern.search(file_line) is not None:
-            OUTPUT.append((line, file_line.rstrip('\n')))
-    for answer in OUTPUT:
-        count += 1
-        with open('OUTPUT.txt', 'a') as file_answer:
-            file_answer.writelines(str(count)+' :')
-            file_answer.writelines(answer[1]+'\n')
-print(count)
+cycle = int(input("Enter the number of words to search :"))
+for i in range(0, cycle):
+    count = 0
+    line = 0
+    OUTPUT = []
+    word = str(input("Enter word {} :".format(i+1)))
+    pattern = re.compile(word, re.I)
+    with open('Input_file.txt', 'rt') as file_info:
+        for file_line in file_info:
+            line += 1
+            if pattern.search(file_line) is not None:
+                OUTPUT.append((line, file_line.rstrip('\n')))
+        for answer in OUTPUT:
+            count += 1
+            with open("{}.txt".format(word), 'a') as file_answer:
+                file_answer.writelines(str(count)+' :')
+                file_answer.writelines(answer[1]+'\n')
+    print(count)
