c0b47bce2d3c01aefe4fdb959fcf613afcfaa2f3
99003762 <78848692+99003762@users.noreply.github.com>
2021-02-25 09:59:56 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,37 +1,37 @@
 import re
 
 
-class person:
-    def __init__(self, cycle):
-        self.cycle = cycle
+class Find:
+    def __init__(self, itr):
+        self.itr = itr
 
-    def myfunc(self):
-        for i in range(0, self.cycle):
+    def my_function(self):
+        for i in range(self.itr):
             count = 0
             line = 0
-            str1 = 0
-            word = str(input("Enter word {} :".format(i+1)))
+            word = str(input("Enter word {} : ".format(i+1)))
             with open("{}.txt".format(word), 'w') as file_answer:
                 with open('Input_file.txt', 'rt') as file_info:
                     for file_line in file_info:
                         line += 1
                         file_line = re.sub(r"\W", " ", file_line)
-                        L = (file_line).split()
-                        l = list(L)
-                        for j in range(len(l)):
-                            if word.lower() == l[j].lower():
-                                if j > 0 and j < (len(l)-1):
+                        k = file_line.split()
+                        for j in range(len(k)):
+                            if word.lower() == k[j].lower():
+                                n= line
+                                if j > 0 and j < (len(k)-1):
                                     count += 1
-                                    str1 = (l[j-1]+" "+l[j]+" "+l[j+1]+'\n')
-                                    file_answer.writelines(str(count)+' :')
-                                    file_answer.write(str1)
-                                    print(str1)
+                                    str1 = ('\n'+k[j-1]+" "+k[j]+" "+k[j+1])
+                                    #file_answer.writelines(str(count)+' :')
+                                    file_answer.write((str1+" --------(IS FOUND AT): "+str(n)))
                                 else:
-                                    file_answer.writelines(str(count+1)+' :')
-                                    file_answer.write(l[j]+'\n')
+                                    #file_answer.writelines(str(count+1)+' :')
+                                    file_answer.write('\n'+k[j]+" --------(IS FOUND AT): "+ str(n))
                                     count += 1
             print("total line count", count)
             f = open("{}.txt".format(word), 'a')
-            f.write("Total number of occurence:"+str(count))
-p1 = person(int(input("Enter the number of words")))
-p1.myfunc()
+            f.write('\n'+"Total number of occurrence:"+str(count))
+
+
+p1 = Find(int(input("Enter the number of words: ")))
+p1.my_function()
