f22db73aab0f5a7704cbd49ef676574a31cb04c5
99003762 <78848692+99003762@users.noreply.github.com>
2021-02-26 10:08:35 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -5,12 +5,14 @@ class Find:
     def __init__(self, itr):
         self.itr = itr
 
+# function to search the given word in given input_text file
     def my_function(self):
         for i in range(self.itr):
             count = 0
             line = 0
-            word = str(input("Enter word {} : ".format(i+1)))
+            word = str(input("Enter word {} : ".format(i+1)))  # word to search
             with open("{}.txt".format(word), 'w') as file_answer:
+                # opening input file in read text mode
                 with open('Input_file.txt', 'rt') as file_info:
                     for file_line in file_info:
                         line += 1
@@ -22,6 +24,7 @@ class Find:
                                 t = " -----(is found at): "
                                 if 0 < j < (len(k)-1):
                                     count += 1
+                                    # to get preceeding and succeeding word
                                     y = k[j-1] + " " + k[j] + " " + k[j+1]
                                     str1 = ('\n' + y)
                                     file_answer.write((str1 + t + str(n)))
@@ -32,6 +35,6 @@ class Find:
             f = open("{}.txt".format(word), 'a')
             f.write('\n'+"Total number of occurrence:"+str(count))
 
-
+# object creation for the class "find"
 p1 = Find(int(input("Enter the number of words: ")))
-p1.my_function()
+p1.my_function()  # method call
